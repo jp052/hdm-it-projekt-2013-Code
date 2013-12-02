896a154146ea2f7f697adb575d243e28c9ca0830
jp052 <jp052@hdm-stuttgart.de>
2013-12-03 00:11:42 +0100

    Umbenannt von IT-Projekt-Code zu StundenplanTool

--- IT-Projekt Code/src/de/hdm/gruppe3/stundenplantool/client/StundenplanTool.java
+++ IT-Projekt Code/src/de/hdm/gruppe3/stundenplantool/client/StundenplanTool.java
@@ -20,7 +20,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class IT_Projekt_Code implements EntryPoint {
+public class StundenplanTool implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
